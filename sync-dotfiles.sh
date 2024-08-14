3aa1fc481038bf378c5b40b9746012e71ce56142
fex0r <felipebelletti1@gmail.com>
2024-08-14 15:31:57 -0300

    Sync dotfiles

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-configFiles=($HOME/.config/kitty/kitty.conf $HOME/.config/yay/config.json)
+configFiles=($HOME/.config/kitty/kitty.conf $HOME/.config/yay/config.json $HOME/.config/wezterm)
 
-cp ${configFiles[@]} .
+cp -r ${configFiles[@]} .
 git add .
 git commit -m "Sync dotfiles"
 git push origin master
